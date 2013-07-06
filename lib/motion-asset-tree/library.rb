34f7efffd1ead43549f6b66b13dde464fa0d110a
akahige <akahigeg@gmail.com>
2013-07-06 16:38:43 +0900

    lib

--- lib/motion-asset-tree/library.rb
+++ lib/motion-asset-tree/library.rb
@@ -1,37 +1,11 @@
 # -*- encoding : utf-8 -*-
 
 class MotionAssetTree
-  attr_accessor :groups
   def initialize
-    @groups = Groups.new
+    @al_asset_library = ALAssetsLibrary.new
   end
 
-  class Groups < Array
-    def find_by_url(group_url)
-      l = ALAssetsLibrary.new
-      l.groupForURL(
-        group_url, 
-        resultBlock: lambda { |group|
-          callback.call(group, nil)
-        },
-        resultBlock: lambda { |error|
-          callback.call(nil, error)
-        }
-      )
-      # valurForProperty ALAssetsGroupPropertyURL
-    end
-
-    def create(name, &callback)
-      l = ALAssetsLibrary.new
-      l.addAssetsGroupAlbumWithName(
-        name, 
-        resultBlock: lambda { |group|
-          callback.call(group, nil)
-        },
-        failureBlock: lambda { |error|
-          callback.call(nil, error)
-        }
-      )
-    end
+  def groups
+    @groups ||= Groups.new(@al_asset_library)
   end
 end
