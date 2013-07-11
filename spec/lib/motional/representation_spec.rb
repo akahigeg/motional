4093fa472d2bb0de2e84b79316348468ed4d1b8c
akahige <akahigeg@gmail.com>
2013-07-11 22:15:59 +0900

    improve Representation spec

--- spec/lib/motional/representation_spec.rb
+++ spec/lib/motional/representation_spec.rb
@@ -1,11 +1,46 @@
 # -*- encoding : utf-8 -*-
 
 describe MotionAL::Representation do
-  # data
-  # cg_image
-  # filename
-  # UTI
-  # metadata
-  # url
-  # 
+
+  before do
+    @rep = MotionAL.library.saved_photos.assets.first.rep
+  end
+
+  describe ".data" do
+    it "should return NSConcreteData(kind of NSData)" do
+      @rep.data.should.kind_of NSData
+    end
+  end
+
+  describe ".cg_image" do
+    it "should return something except nil. maybe CGImageRef" do
+      @rep.cg_image.should.not.nil
+    end
+  end
+
+  describe ".filename" do
+    it "should return filename" do
+      @rep.filename.should.match /jpg$|png/i
+    end
+  end
+
+  # what's UTI?
+  describe ".UTI" do
+    it "should return public.jpeg?" do
+      @rep.UTI.should.equal 'public.jpeg'
+    end
+  end
+
+  describe ".metadata" do
+    it "should return metadata" do
+      @rep.metadata.should.kind_of Hash
+    end
+
+  end
+
+  describe ".url" do
+    it "should return NSURL object" do
+      @rep.url.should.kind_of NSURL
+    end
+  end
 end
